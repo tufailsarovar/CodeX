6f65b25ce0d2d2c9690ee9ed8f6b639e0a40e296
Tufail Sarovar <sarovar143786@gmail.com>
2026-01-18 00:27:30 +0530

    axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://code-x-eight-murex.vercel.app",
+    "https://code-x-eight-murex.vercel.app/api",
   withCredentials: true,
   timeout: 30000,
 });
