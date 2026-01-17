2c2be306bd235872ee74ac459eb21ba88132928d
Tufail Sarovar <sarovar143786@gmail.com>
2026-01-18 01:53:48 +0530

    deleted

--- server/routes/adminAuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { adminLogin } from "../controllers/adminAuthController.js";
-
-const router = express.Router();
-
-router.post("/login", adminLogin);
-
-export default router;
--- server/routes/contactRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { sendContact } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", sendContact);
-
-export default router;
