e23c4db2ad4a397b6635b8dc7c9d89dbee91e887
Tufail Sarovar <sarovar143786@gmail.com>
2026-01-17 20:54:17 +0530

    axios timeout

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  timeout: 8000, // 🔥 prevents hanging requests
+  timeout: 30000, // 🔥 prevents hanging requests
 });
 
 // Attach token
